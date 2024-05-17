14d6344e5bea4bba738720ede6609f7c5610de4a
Jason <jgraves96@gmail.com>
2024-05-17 10:21:30 -0400

    Added the Assignment 2 class

---
+++ Assignment 2/main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/** Jason Graves
+ *  Fibonnaci calculator,
+ *  this class calculates a certain number in the fib sequence
+ *  change X to what number you want to find.
+ */
+
+public class main {
+    public static void main(String args[]){
+        System.out.println(Fibonacci(10));
+        /**
+         * @param
+         */
+    }
+
+    private static int Fibonacci(int x) {
+        int result = 0;
+        int no1=0, no2=1;
+
+        for(int i=0;i<x-1;i++){
+            result = no1+no2;
+
+            no1 = no2;
+            no2 = result;
+        }
+        return result;
+        /**
+         * @result
+         */
+    }
+}
